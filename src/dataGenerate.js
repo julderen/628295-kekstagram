b262045c3d7fe01e26fb0802b9fc0f05be2bb83b
deniskorokhodkin <korokhodkindenis@gmail.com>
2018-02-18 15:26:34 +0300

    Правят ПР

--- src/dataGenerate.js
+++ src/dataGenerate.js
@@ -1,7 +1,75 @@
-const defaultData = require(`./constants/defaultData`);
+const {generateRandomNumberInRange, generateRandomString} = require(`./utils/dataGenerateUtils`);
+const {
+  EFFECTS,
+  WORDS,
+  MAX_SCALE,
+  MAX_LIKES,
+  MAX_LENGTH_DESCRIPTION,
+  MAX_COUNT_HASH_TAGS,
+  MAX_COUNT_COMMENTS,
+  MAX_URL,
+  MAX_LENGTH_COMMENT
+} = require(`./constants/dataGenerateConstants`);
 
 module.exports = {
+  generateUrl() {
+    return `https://picsum.photos/600/?${generateRandomNumberInRange(MAX_URL)}`;
+  },
+
+  generateScale() {
+    return generateRandomNumberInRange(MAX_SCALE);
+  },
+
+  generateEffect() {
+    return EFFECTS[generateRandomNumberInRange(EFFECTS.length - 1)];
+  },
+
+  generateLikes() {
+    return generateRandomNumberInRange(MAX_LIKES);
+  },
+
+  generateHashTags() {
+    const countHashTags = generateRandomNumberInRange(MAX_COUNT_HASH_TAGS);
+    let hashTags = [];
+    let hashTag = ``;
+
+    for (let i = 0; i < countHashTags; i++) {
+      do {
+        const indexHashTag = generateRandomNumberInRange(WORDS.length - 1);
+
+        hashTag = `#${WORDS[indexHashTag]}`;
+      } while (hashTags.includes(hashTag));
+
+      hashTags.push(hashTag);
+    }
+
+    return hashTags;
+  },
+
+  generateDescription() {
+    return generateRandomString(MAX_LENGTH_DESCRIPTION, WORDS);
+  },
+
+  generateComments() {
+    let comments = [];
+    const countComments = generateRandomNumberInRange(MAX_COUNT_COMMENTS);
+
+    for (let i = 0; i < countComments; i++) {
+      comments.push(generateRandomString(MAX_LENGTH_COMMENT, WORDS));
+    }
+
+    return comments;
+  },
+
   generateEntity() {
-    return defaultData;
+    return {
+      url: this.generateUrl(),
+      scale: this.generateScale(),
+      effect: this.generateEffect(),
+      hashtags: this.generateHashTags(),
+      description: this.generateDescription(),
+      likes: this.generateLikes(),
+      comments: this.generateComments()
+    };
   }
 };
--- test/dataGenerate.test.js
+++ test/dataGenerate.test.js
@@ -1,11 +1,126 @@
+const {describe, it} = require(`mocha`);
 const assert = require(`assert`);
-const defaultData = require(`../src/constants/defaultData`);
 const dataGenerate = require(`../src/dataGenerate`);
 
-describe(`data-generate`, () => {
-  describe(`generateEntity()`, () => {
-    it(`should return defaultData`, () => {
-      assert.equal(dataGenerate.generateEntity(), defaultData);
+describe(`dataGenerate generateEntity()`, () => {
+  const entity = dataGenerate.generateEntity();
+
+  describe(`url`, () => {
+    const url = entity.url;
+
+    it(`should return string`, () => {
+      assert.equal(typeof (url), `string`);
+    });
+
+    it(`should start with 'http://' or 'https://'`, () => {
+      assert(!url.startsWith(`http://`) || !url.startsWith(`https://`));
+    });
+  });
+
+  describe(`scale`, () => {
+    const scale = entity.scale;
+
+    it(`should be number`, () => {
+      assert.equal(typeof (scale), `number`);
+    });
+
+    it(`should be more or equal 0`, () => {
+      assert(scale >= 0);
+    });
+
+    it(`should be less or equal 100`, () => {
+      assert(scale <= 100);
+    });
+  });
+
+  describe(`effect`, () => {
+    const effect = entity.effect;
+    const EFFECTS = [`chrome`, `sepia`, `marvin`, `phobos`, `heat`];
+
+    it(`should be string`, () => {
+      assert.equal(typeof (effect), `string`);
+    });
+
+    it(`should be one of world ${EFFECTS.join(`, `)}`, () => {
+      assert(EFFECTS.includes(effect));
+    });
+  });
+
+  describe(`hashtags`, () => {
+    const hashtags = entity.hashtags;
+
+    it(`should be array`, () => {
+      assert(Array.isArray(hashtags));
+    });
+
+    it(`hashtags should be less or equal 5`, () => {
+      assert(hashtags.length <= 5);
+    });
+
+    it(`hashtag should be start with '#'`, () => {
+      for (let i = 0; i < hashtags.length; i++) {
+        assert(hashtags[i].startsWith(`#`));
+      }
+    });
+
+    it(`hashtag should be haven't ' '`, () => {
+      for (let i = 0; i < hashtags.length; i++) {
+        assert(!hashtags[i].includes(` `));
+      }
+    });
+
+    it(`hashtag should be word max length 20`, () => {
+      for (let i = 0; i < hashtags.length; i++) {
+        assert(hashtags[i].length <= 20 + 1);
+      }
+    });
+
+    it(`hashtags should be haven't same words`, () => {
+      for (let i = 0; i < hashtags.length - 1; i++) {
+        assert(hashtags.indexOf(hashtags[i], i + 1) === -1);
+      }
+    });
+  });
+
+  describe(`description`, () => {
+    const description = entity.description;
+
+    it(`should return string`, () => {
+      assert.equal(typeof (description), `string`);
+    });
+
+    it(`description length should be less or equal 140`, () => {
+      assert(description.length <= 140);
+    });
+  });
+
+  describe(`likes`, () => {
+    const likes = entity.likes;
+
+    it(`should return number`, () => {
+      assert.equal(typeof (likes), `number`);
+    });
+
+    it(`should be more or equal 0`, () => {
+      assert(likes >= 0);
+    });
+
+    it(`should be less or equal 1000`, () => {
+      assert(likes <= 1000);
+    });
+  });
+
+  describe(`comments`, () => {
+    const comments = entity.comments;
+
+    it(`should return array`, () => {
+      assert(Array.isArray(comments));
+    });
+
+    it(`comment should be word max length 140`, () => {
+      for (let i = 0; i < comments.length; i++) {
+        assert(comments[i].length <= 140);
+      }
     });
   });
 });
